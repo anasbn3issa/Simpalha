e2a0cf862388f001c84fbd086318edb4ee599de1
Arij Zitouni <arijzt8@gmail.com>
2021-04-28 18:20:29 +0100

    last fixes

--- Symfony4/config/bundles.php
+++ Symfony4/config/bundles.php
@@ -24,4 +24,5 @@ return [
     Vangrg\ProfanityBundle\VangrgProfanityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
 ];
