2240c490745e9df51a71cd5f44f45a9bd11e8935
WajdiSd <wajdi.sadouki@esprit.tn>
2021-04-28 21:21:34 +0100

    pres

--- Symfony4/config/bundles.php
+++ Symfony4/config/bundles.php
@@ -25,4 +25,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
