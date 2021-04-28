ccc5cdc9cb58b4e2624b1415c496d5b820a988f1
Parsath <bilel.taktak@esprit.tn>
2021-04-28 17:39:47 +0200

    added uxchart js and implemented it

--- Symfony4/config/bundles.php
+++ Symfony4/config/bundles.php
@@ -22,4 +22,6 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     IndyDevGuy\WikiBundle\WikiBundle::class => ['all' => true],
     Vangrg\ProfanityBundle\VangrgProfanityBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
